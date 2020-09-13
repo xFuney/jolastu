fd6152036256fb05bb15eacd8f52405baa6a73d6
xFuney <56867550+xFuney@users.noreply.github.com>
2020-09-13 19:08:55 +0100

    introduce pin archival, fix vulnerabilities as found by dependabot

--- bot/commands/utils.js
+++ bot/commands/utils.js
@@ -25,6 +25,73 @@ module.exports.commands = {
                 //let test = api_db.get("101", "user_warns", {user_id: 10101})
             }
         },
+        "archive-pins": {
+            pretty_name: "archive-pins",
+            description: "Archive pins from the channel where this mesage is posted in the provided channel ID.",
+            execute: async function(message, Args, client, Discord) {
+                if (!Args[1]) {
+                    message.channel.send("Error: Please provide a channel ID.")
+                    return;
+                }
+                //console.log(message)
+                
+                if (message.guild.members.cache.get(message.author.id).hasPermission('KICK_MEMBERS')) {
+                    // Valid. Now start archival.
+                    const archiveChannel = message.guild.channels.cache.find(channel => channel.id === Args[1]);
+                    message.channel.messages.fetchPinned().then(function(pinnedMessages){
+                        let pinnedArray = pinnedMessages.array()
+                        for ( const n in pinnedArray ) {
+                            var img = "no";
+                            var attachments = "";
+                            var embeds = "";
+                            var attachmentsI = 1;
+                            var embedsI = 1;
+
+                            //if (message.attachments) {
+                                pinnedArray[n].attachments.forEach(attachment => {
+                                    // do something with the attachment
+                                    attachments += "[Attachment " + attachmentsI + " - " + attachment.name + "](" + attachment.url + ")\n"
+                                });
+                                
+                                //console.log(message)
+                                pinnedArray[n].embeds.forEach(embed => {
+                                    // do something with the attachment
+                                    if (embed.type == "image") {
+                                        embeds += "[Embed " + embedsI + " (Image)](" + embed.url + ")\n"
+                                    } else if (embed.type == "gifv") {
+                                        embeds += "[Embed " + embedsI + " (GIF)](" + embed.url + ")\n"
+                                    } else if (embed.type == "article") {
+                                        embeds += "[Embed " + embedsI + " (Article)](" + embed.url + ")\n"
+                                    } else if (embed.type == "video") {
+                                        embeds += "[Embed " + embedsI + " (Video)](" + embed.url + ")\n"
+                                    } else if (embed.type == "link") {
+                                        embeds += "[Embed " + embedsI + " (Link)](" + embed.url + ")\n"
+                                    }
+                                });
+
+
+                            //}
+
+                            //console.log(pinnedArray[n].content)
+                            var msgEmbed = new Discord.MessageEmbed()
+                            .setColor('0000ff')
+                            .setAuthor(pinnedArray[n].author.tag, pinnedArray[n].author.avatarURL())
+                            .setDescription(pinnedArray[n].content)
+                            .setTimestamp(pinnedArray[n].createdTimestamp)
+                            .addField('Attachments', attachments || "*None*")
+                            .addField('Embeds', embeds || "*None*")
+                            .setFooter('Brought to you by ' + BOT_CONFIG.bot_name);
+
+                            //if (pinnedArray[n].embeds.array().length > 1) {
+                                //msgEmbed.setImage(img)
+                            //}
+
+                            archiveChannel.send(msgEmbed)
+                        }
+                    })
+                }
+            }
+        },
         "help": {
             pretty_name: "help",
             description: "Get help.",
